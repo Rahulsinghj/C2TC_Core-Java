5a4c23ce25cd29a0f787cb714f318b57aa9947f5
rahul <rahul@MSI>
2022-08-08 23:22:08 +0530

    First commit

---
+++ Fundamentals/src/com/cg/fundamentals/DoWhile.java
@@ -0,0 +1,16 @@
+package com.cg.fundamentals;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i =0;
+		do {
+		System.out.print(i+ "");
+		i++;
+		}while(i<10);
+		}
+		
+
+	}
+
