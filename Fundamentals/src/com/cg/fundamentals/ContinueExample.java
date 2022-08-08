309a82780f26cdaf21905cc99ec0a5fd78ae195f
rahul <rahul@MSI>
2022-08-08 21:26:20 +0530

    First Commit

---
+++ Fundamentals/src/com/cg/fundamentals/ContinueExample.java
@@ -0,0 +1,20 @@
+package com.cg.fundamentals;
+
+public class ContinueExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int limit= 10;
+		int sum=0;
+		for(int i =1;i<=limit;i++) {
+		if(i%4==0) {
+		continue;
+		}
+		sum=sum+i; //33+10 =43
+		}
+		System.out.println(sum);
+		}
+
+	}
+
+
