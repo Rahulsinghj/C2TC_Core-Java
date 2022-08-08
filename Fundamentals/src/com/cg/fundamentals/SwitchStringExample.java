fbe28000bedbd72f4e94dcc28ba35f9d155837e3
rahul <rahul@MSI>
2022-08-08 21:40:52 +0530

    Firstcommit

---
+++ Fundamentals/src/com/cg/fundamentals/SwitchStringExample.java
@@ -0,0 +1,25 @@
+package com.cg.fundamentals;
+
+public class SwitchStringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubString currentDay=&quot;Thursday&quot;;
+		String currentDay="Thursday";
+		switch(currentDay) {
+		case "Monday":
+		case "Tuesday":
+		case "Wednesday":
+		System.out.println("boring");
+		case "Thursday":
+		System.out.println("getting better");
+		break;
+		case "Friday":
+		System.out.println("much better");
+		case "Saturday":
+		case "Sunday":
+		}
+		
+
+	}
+
+}
