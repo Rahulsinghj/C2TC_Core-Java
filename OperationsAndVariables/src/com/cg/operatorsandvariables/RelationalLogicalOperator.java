cfd9e0a39225db090067e10f7a8d1a9513d30ec6
rahul <rahul@MSI>
2022-08-14 13:59:34 +0530

    first commit

--- OperationsAndVariables/src/com/cg/operatorsandvariables/RelationalLogicalOperator.java
+++ OperationsAndVariables/src/com/cg/operatorsandvariables/RelationalLogicalOperator.java
@@ -4,7 +4,22 @@ public class RelationalLogicalOperator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Random rand = new Random();
+		int i = rand.nextInt(100);
+		int j = rand.nextInt(100);
+		System.out.println("i =  + i");
+		System.out.println("j =  + j");
+		System.out.println(i  j is  + (i  j));
+		System.out.println("i  j is " + (i< j));
+		System.out.println("i = j is " + (i <= j));
+		System.out.println("i = j is " + (i &lt;= j));
+		System.out.println(&quot;i == j is &quot; + (i == j));
+		System.out.println(&quot;i != j is &quot; + (i != j));
+		System.out.println(&quot;(i &lt; 10) &amp;&amp; (j &lt; 10) is &quot; + ((i &lt; 10)
+		&amp;&amp; (j &lt; 10)));
+		System.out.println(&quot;(i &lt; 10) || (j &lt; 10) is &quot; + ((i &lt; 10)
+		|| (j &lt; 10)));
+		}
+		}
 
-	}
-
-}
+		public class TernaryOperator {
